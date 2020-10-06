4a9e0231a372037af9e67f50315c9f33f265b786
iaw51286949 <iaw51286949@h19.informatica.escoladeltreball.org>
2020-10-06 19:48:52 +0200

    c2

--- HelloWorldv2/src/com/dav/isma/HelloWorldv2.java
+++ HelloWorldv2/src/com/dav/isma/HelloWorldv2.java
@@ -5,6 +5,8 @@ public class HelloWorldv2 {
 	public static void main(String[] args) {
 		System.out.println("Isma");
 		System.out.println("Hello World");
+		System.out.println("Linea nueva");
+		System.out.println("Linea nueva 2");
 
 	}
 
