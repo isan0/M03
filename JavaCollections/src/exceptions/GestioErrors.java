1c185589c9ed48104f2648fe29c1ddba6ecba76e
iaw51286949 <iaw51286949@localhost>
2021-01-18 18:56:17 +0100

    exeption

---
+++ JavaCollections/src/exceptions/GestioErrors.java
@@ -0,0 +1,44 @@
+package exceptions;
+
+public class GestioErrors {
+	
+	public static void met01() {
+		int v[] = {1,2,3};
+		int elem;
+		
+		try {
+			elem = v[5];
+		}
+		
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Codi del catch");
+		}
+		
+		finally {
+			System.out.println("Final del programa");
+		}
+	}
+	
+	public static void met02() {
+		String cadena = "hola";
+		try {
+			System.out.println(cadena.charAt(5));
+		}
+		
+		catch(Exception e) {
+			System.out.println("Codi del catch");
+		}
+		
+		finally {
+			System.out.println("Final del programa");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		met01();
+		met02();
+	}
+
+}
--- JavaCollections/src/exceptions/InvalidUserIdException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class InvalidUserIdException extends Exception {
-	@Override
-	public String toString() {
-		return "Invalid user id entered";
-	}
-}
