b43ff18200c2d32451df2fe819babbc5969c3b20
Ismael Aveiga <ismael.aveiga11@gmail.com>
2021-01-27 11:01:24 +0100

    master

--- JavaCollections/src/stream/Ej3.java
+++ JavaCollections/src/stream/Ej3.java
@@ -1,10 +1,41 @@
 package stream;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Ej3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		File Original = new File("ej3.txt");
+		File Destino = new File("ej3_2.txt");
+		try {
+			copiar(Original, Destino);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	private static void copiar(File Original, File Destino) throws IOException {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		try {
+			in = new FileInputStream(Original);
+			out = new FileOutputStream(Destino);
+			int x;
+			while ( (x=in.read())>-1){
+				out.write(x);
+			}
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			in.close();
+			out.close();
+		}
+		
 	}
 
 }
---
+++ JavaCollections/src/stream/Ej4.java
@@ -0,0 +1,18 @@
+package stream;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Ej4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lectura = "ej4.txt";
+		String escritura= "ej4_2";
+		
+		InputStream fInputStream = null;
+		OutputStream fOutputStream = null;
+		
+	}
+
+}
